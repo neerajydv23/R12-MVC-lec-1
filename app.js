badf7b31d1e64409fe778f330c2522443302b4d5
Neeraj <yneeraj082r@gmail.com>
2024-03-13 17:29:04 +0530

    added mongo atlas

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ require('dotenv').config({path:"./.env"})
 const express = require('express')
 const app = express()
 
+require('./models/dbconfig.js').connectDB();
+
+
+
+
 
 app.listen(process.env.PORT,()=>{
     console.log( `Server running on PORT ${process.env.PORT}`);
